a788280b50cd2d10f68ca1918eb323b6e5335a21
Goran Peretin <goran.peretin@gmail.com>
2015-10-25 15:37:09 -0700

    Fix find-divisor for exercise 1.22

--- 1.2/common.scm
+++ 1.2/common.scm
@@ -1,14 +1,16 @@
+(define (square a)
+  (* a a))
 
 (define (smallest-divisor n)
   (find-divisor n 2))
 
 (define (find-divisor n test-divisor)
-  (cond ((is_divisible? n test-divisor) test-divisor)
-        (else
-          (find-divisor n (+ 1 test-divisor)))))
+  (cond ((> (square test-divisor) n) n)
+        ((divides? test-divisor n) test-divisor)
+        (else (find-divisor n (+ test-divisor 1)))))
 
-(define (is_divisible? a b)
-  (= 0 (remainder a b)))
+(define (divides? a b)
+  (= (remainder b a) 0))
 
 (define (prime? n)
   (= (smallest-divisor n) n))
